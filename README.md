58652f3d913cd6a0074d99fa82afab2c2f74d38c
Morales-BA <moralesdafro@gmail.com>
2018-10-14 15:29:46 -0500

    initial commit server crashes, need help

---
+++ README.md
@@ -0,0 +1,1 @@
+Makes login page and landing page. On successful login goes to landing, failed login circles back to login.
---
+++ src/main/java/com/revature/servlet/LandingServlet.java
@@ -0,0 +1,18 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/landing")
+public class LandingServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+}
